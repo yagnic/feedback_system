fb3b4d3e2b69f8ca2f888556d4a83a292ee22bdd
Yagnic Vanam <yagnicvanam@Yagnics-MacBook-Air.local>
2024-11-08 18:50:58 +0530

    Updates

---
+++ precache-manifest.2d375112494b29422a3efc1dc48a7600.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49dee38f87c61c7fddff56523947d75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "413762112267af47ea2d",
+    "url": "/static/css/main.1bcd9870.chunk.css"
+  },
+  {
+    "revision": "b522702c984081de7748",
+    "url": "/static/js/2.3bea3b5a.chunk.js"
+  },
+  {
+    "revision": "413762112267af47ea2d",
+    "url": "/static/js/main.ba6bea45.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
